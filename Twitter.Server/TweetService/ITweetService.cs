5f4a4cf81ae28f9b9bc63ec2e70718a56b05a303
eddyiiz <eddyxsayf@gmail.com>
2024-08-10 08:26:06 +0200

    TweetService Methods

--- Twitter.Server/TweetService/ITweetService.cs
+++ Twitter.Server/TweetService/ITweetService.cs
@@ -2,4 +2,9 @@
 
 public interface ITweetService
 {
+    Task<ServiceResponse<List<Tweet>>> GetTweets();
+    Task<ServiceResponse<Tweet>> GetTweet(int id);
+    Task<ServiceResponse<Tweet>> CreateTweet(Tweet newTweet);
+    Task<ServiceResponse<Tweet>> UpdateTweet(int id, Tweet UpdatedTweet);
+    Task<ServiceResponse<bool>> DeleteTweet(int id);
 }
--- Twitter.Server/TweetService/TweetService.cs
+++ Twitter.Server/TweetService/TweetService.cs
@@ -1,5 +1,124 @@
-﻿namespace Twitter.Server.TweetService;
+﻿
+namespace Twitter.Server.TweetService;
 
 public class TweetService : ITweetService
 {
+    private readonly DataContext _context;
+
+    public TweetService(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ServiceResponse<List<Tweet>>> GetTweets()
+    {
+        var response = new ServiceResponse<List<Tweet>>();
+        try
+        {
+            response.Data = await _context.Tweets.ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Tweet>> GetTweet(int id)
+    {
+        var response = new ServiceResponse<Tweet>();
+        try
+        {
+            var tweet = await _context.Tweets.FindAsync(id);
+
+            if (tweet == null)
+            {
+                response.Success = false;
+                response.Message = "Not Found";
+            }
+
+            response.Data = tweet;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Tweet>> CreateTweet(Tweet newTweet)
+    {
+        var response = new ServiceResponse<Tweet>();
+        try
+        {
+            _context.Tweets.Add(newTweet);
+
+            await _context.SaveChangesAsync();
+
+            response.Data = newTweet;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Tweet>> UpdateTweet(int id, Tweet UpdatedTweet)
+    {
+        var response = new ServiceResponse<Tweet>();
+        try
+        {
+            var tweet = await _context.Tweets.FindAsync(id);
+
+            if (tweet == null)
+            {
+                response.Success = false;
+                response.Message = "Not found";
+            }
+
+            tweet.Title = UpdatedTweet.Title;
+            tweet.Description = UpdatedTweet.Description;
+
+            response.Data = UpdatedTweet;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<bool>> DeleteTweet(int id)
+    {
+        var response = new ServiceResponse<bool>();
+        try
+        {
+            var tweet = await _context.Tweets.FindAsync(id);
+
+            if (tweet == null)
+            {
+                response.Success = false;
+                response.Message = "Not found";
+            }
+
+            _context.Tweets.Remove(tweet);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
 }
